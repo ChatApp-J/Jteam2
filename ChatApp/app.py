e8ddcdfe957bac714811c1a5ea1fab1bdb9627a8
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2025-08-08 22:54:16 +0900

    サインアップ完成（仮)

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+import re
+import uuid
+import hashlib
+from models import User
+import os
+from datetime import timedelta
 
 
+EMAIL_PATTERN = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._%+-]+\.[a-zA-z]{2,}$"
+SESSION_DAYS= 30
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY'or uuid.uuid4().hex)#.envから秘密鍵を読み込む　なかったら生成する
+app.permanent_session_lifetime = timedelta(days=SESSION_DAYS)# セッションの日数を計算
 
 
 #トップページの表示
@@ -17,9 +28,44 @@ def signup ():
 
 
 #サインアップの処理
-
-
-#ログインページの表示
+@app.route('/signup', methods = ['POST'])
+def signup_create():#関数を制作しrequestを使用し登録内容を取得
+    name = request.form.get('name')
+    email = request.form.get("email")
+    nickname =request.form.get("nickname")
+    password = request.form.get("password")
+    confirm_password = request.form.get("confirm_password")
+    if name==" " or email==" " or nickname ==  " " or password == " " or confirm_password == " ":
+        flash("空のフォームがあります。記入してください。")
+        
+    elif password != confirm_password:
+        flash("パスワードが違います")
+       
+    elif re.fullmatch (EMAIL_PATTERN,email) is None:
+        flash("メールアドレスが正しくありません")
+        
+    else:
+        uid = uuid.uuid4()
+        #任意の文字列を加えることでより複雑なハッシュ化をする　salt
+        salt = "Solty"
+        salt_password = password+salt
+        password = hashlib.sha256(salt_password.encode("utf-8")).hexdigest()
+        registered_user = User.find_by_email(email)#データベースに同じアドレスを探す
+            
+        if registered_user != None:#もし同じアドレスがあったら
+                flash('既に登録されているようです')
+        else:
+            User.create(uid, name, email, nickname,password)
+            UserId = str(uid)
+            session['uid'] = UserId
+            return redirect(url_for('channels_view'))
+    return redirect(url_for('signup'))
+        
+    
+    
+    
+    
+    #ログインページの表示
 #画面の取得なのでGETメソッド
 @app.route('/login', methods=['GET'])
 #ログイン画面の定義
@@ -35,6 +81,7 @@ def login_view():
 
 
 #チャンネル一覧ページ（ホーム？）の表示
+
 
 
 # チャンネル（ルーム？）の作成
--- docker/MySQL/init.sql
+++ docker/MySQL/init.sql
@@ -1,37 +1,22 @@
+DROP DATABASE IF EXISTS j_team;
+DROP USER IF EXISTS 'j_team';
 
-DROP DATABASE chatapp;
-DROP USER 'testuser';
 
-CREATE USER 'testuser' IDENTIFIED BY 'testuser';
-CREATE DATABASE chatapp;
-USE chatapp
-GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
+CREATE USER 'j_team' IDENTIFIED BY 'j_team';  -- データベースを操作するuserを作成
+CREATE DATABASE j_team; -- データベースを作成　名前はChatApp
 
-CREATE TABLE users (
+USE j_team; -- ここからはChatAppのデータベースを操作する
+
+GRANT ALL PRIVILEGES ON j_team.* TO j_team;  -- ChatAppでの全権限を付与
+
+CREATE TABLE users(
     uid VARCHAR(255) PRIMARY KEY,
-    user_name VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)  NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+    nickname VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
-CREATE TABLE channels (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    uid VARCHAR(255) NOT NULL,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    abstract VARCHAR(255),
-    FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE
-);
 
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    uid VARCHAR(255) NOT NULL,
-    cid INT NOT NULL,
-    message TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE,
-    FOREIGN KEY (cid) REFERENCES channels(id) ON DELETE CASCADE
-);
-
-INSERT INTO users(uid, user_name, email, password) VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
